1e1c656def91c1e14b120ec285ec219da36787da
yomoazu <0806.kimiko@gmail.com>
2022-06-29 15:44:15 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'protospaces/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "prototypes#index"
-resources :prototypes, only: [:index, :new,:create,:destroy]
-resources :users, only: :show
+  root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create 
+  end
+  resources :users, only: :show
 end
